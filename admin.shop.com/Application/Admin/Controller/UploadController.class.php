679feb1402b8030667175fbfeb957a7db60c6cdd
四哥 <kunx@kunx.org>
2016-01-23 14:26:31 +0800

    七牛云又拍云的配置，以及上传后将地址存到数据库中

--- admin.shop.com/Application/Admin/Controller/UploadController.class.php
+++ admin.shop.com/Application/Admin/Controller/UploadController.class.php
@@ -16,6 +16,7 @@ class UploadController extends \Think\Controller {
      * file：上传成功后的所有的文件信息
      */
     public function index() {
+//        dump($_FILES);
         C('SHOW_PAGE_TRACE', FALSE);
         $data = array(
             'status' => 1,
@@ -25,32 +26,58 @@ class UploadController extends \Think\Controller {
         );
 //        echo json_encode($data);
 
+        $bucket_array = array(
+            'brand'=>'kunx_brand',
+            'goods'=>'kunx_goods',
+            'default'=>'test1011',
+            
+        );
+        $bucket = isset($bucket_array[I('post.category')])?$bucket_array[I('post.category')]:$bucket_array['default'];
         /**
          * 具体执行上文件的保存操作
          */
         $config = array(
-//            'maxSize'  => 3145728, //3M
-            'maxSize'  => 30, //3M
+            'maxSize'  => 3145728, //3M
+//            'maxSize'      => 30, //3M
 //            'savePath' => C('UPLOAD_PATH'),
             'rootPath' => './',
-            'savePath' => 'Uploads/',
+            'savePath'     => 'Uploads/',
             'saveName' => array('uniqid', ''),
-            'exts'     => array('jpg', 'gif', 'png', 'jpeg'),
-            'autoSub'  => true,
-            'subName'  => array('date', 'Ymd'),
-            'mimes'    => array('image/jpeg', 'image/png', 'image/gif'), //允许上传的文件MiMe类型
+            'exts'         => array('jpg', 'gif', 'png', 'jpeg'),
+            'autoSub'      => true,
+            'subName'      => array('date', 'Ymd'),
+            'mimes'        => array('image/jpeg', 'image/png', 'image/gif'), //允许上传的文件MiMe类型
+            //又拍云配置
+//            'driver'       => 'Upyun',
+//            'driverConfig' => array(
+//                'host'     => 'v0.api.upyun.com', //又拍云服务器
+//                'username' => 'kunx', //又拍云操作员用户名
+//                'password' => 'sigexukun', //又拍云操作员密码
+//                'bucket'   => 'test1033', //空间名称
+//                'timeout'  => 90, //超时时间
+//            ),
+            //七牛配置
+            'driver'       => 'Qiniu',
+            'driverConfig' => array(
+                'secrectKey' => 'KBYoPnqTbgX4a65rXNI9f-6_kCKwwnHMSnLOGLNk', //七牛服务器
+                'accessKey' => 'qJHe4wo24q6X6AWSXsv-syl8PkhHjo6i5WXc-to5', //七牛用户
+                'domain'    => '7xnizi.com1.z0.glb.clouddn.com', //七牛密码
+//                'bucket'    => 'test1011', //空间名称
+                'bucket'    => $bucket, //空间名称
+                'timeout'   => 300, //超时时间
+            ),
         );
         $upload = new \Think\Upload($config);
-        $file   = $upload->upload();
+        $file   = $upload->uploadOne($_FILES['logo']);
         if ($file) {
-            $data['file'] = $file;
-        }else{
+            $data['file'] = json_encode($file);
+        } else {
             $data = array(
-                'status'=>0,
-                'msg'=>$upload->getError(),
+                'status' => 0,
+                'msg'    => $upload->getError(),
             );
         }
-//        dump($upload->getError());
+//        dump($upload->getError());itsource   itsource
         echo json_encode($data);
     }
 
